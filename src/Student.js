f65af861a1c49a1ea5c4c10b773128deabb5acdd
Moiz <m.sheikh0324@gmail.com>
2023-03-12 15:09:43 +0500

    Create Student.js file and add props in it

---
+++ src/Student.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Student(props){
+    return (
+    <div>
+    <h1> This is {props.s1}</h1>
+    <h1> This is {props.s2}</h1>
+    </div>
+    )
+}
+
+export default Student;
