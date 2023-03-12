d6560ba3e2680778170a1640f2ddfdd8140e4b8a
Moiz <m.sheikh0324@gmail.com>
2023-03-12 15:10:36 +0500

    Update App.js File and Add the Css .app class + add child component in it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Student from './Student';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Moiz Murad <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Student s1="Moiz Murad" s2="Waheed Murad" />
+    <Student s1="Nazish Waheed" s2="Waheed Murad" />
     </div>
-  );
+  )
 }
 
 export default App;
