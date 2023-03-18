e2d171e05bf4013402c1920888dbc27f52c5403c
Moiz <m.sheikh0324@gmail.com>
2023-03-18 16:59:37 +0500

    updates in App.css and gave spaces in app.js

--- src/App.js
+++ src/App.js
@@ -7,22 +7,28 @@ import Student from './Student.js';
 
 function App() {
 
-  let [count, setcount] = useState(1);
-  let [isday, setday] = useState(false);
+  let [count, setcount] = useState(0);
+  let [isday, setday] = useState(true);
 
   return (
 
     <div className={`${isday ? "box" : "box2"}`}>
     <Student counter = {count}/>
     <h1>Day Time: {isday ? "Moiz" : "Murad"} </h1>
-    <button onClick={()=> {setcount(++count);}}>Count Plus 1</button>
-    <button onClick={() => {setday(!isday)}}>Change</button>
+    
+    <button onClick={()=> {
+      setcount(count = count + 2);}
+      }>Count Plus 2</button>
+    
+    <button onClick={() => {
+      setday(!isday)}
+      }>Change</button>
+
     <button onClick={() => {
       setcount(count = count * 0);
       setday(false);
       }
       }>Reset</button>
-
     </div>
   )
 }
