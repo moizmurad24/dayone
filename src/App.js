0f51f9b61b778ebd28499d1541476f9a4c2f85ca
Moiz <m.sheikh0324@gmail.com>
2023-03-18 22:11:46 +0500

    Reset the files

--- src/App.js
+++ src/App.js
@@ -1,35 +1,8 @@
 import React, {useState} from 'react';
-import './App.css';
-import Student from './Student.js';
-// import Message from './Message';
-
-
 
 function App() {
 
-  let [count, setcount] = useState(0);
-  let [isday, setday] = useState(true);
-
   return (
-
-    <div className={`${isday ? "box" : "box2"}`}>
-    <Student counter = {count}/>
-    <h1>Day Time: {isday ? "Moiz" : "Murad"} </h1>
-    
-    <button onClick={()=> {
-      setcount(count = count + 2);}
-      }>Count Plus 2</button>
-    
-    <button onClick={() => {
-      setday(!isday)}
-      }>Change</button>
-
-    <button onClick={() => {
-      setcount(count = count * 0);
-      setday(false);
-      }
-      }>Reset</button>
-    </div>
   )
 }
 
--- src/Student.js
+++ src/Student.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Student(props){
-    return (
-    <div>
-    <h1> The Number Is: {props.counter}</h1>
-    </div>
-    )
+
 }
 
 export default Student;
