f883d75bf27743570418ef5461eb2cf8504fdcba
Moiz <m.sheikh0324@gmail.com>
2023-03-18 22:14:18 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, {useState} from 'react';
+import './App.css';
+import Student from './Student.js';
+// import Message from './Message';
+
+
 
 function App() {
 
   return (
+    <h1>This is Moiz Murad</h1>
   )
 }
 
